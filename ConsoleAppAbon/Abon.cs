5df56eaa9944132dec7b6c0f0eb0a1505cf2f2d0
Ivan Špeh <ivan.speh@gmail.com>
2021-09-14 17:33:17 +0200

    Abon class added

---
+++ ConsoleAppAbon/Abon.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAbon
+{
+    public class Abon
+    {
+        public string SerialNumber { get; set; }
+    }
+}
