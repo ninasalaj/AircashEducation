ea21d9c232153ad123ce4170f39b7844dc1fd28b
Nina Salaj <nina.salaj.ns@gmail.com>
2021-09-14 17:21:01 +0200

    Add project files.

---
+++ ConsoleAppAbon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppAbon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
